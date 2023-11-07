5104d5606bc8388ffd9f07016f5c366c469dcfb5
pjhaymanalo <pjhaymanalo@gmail.com>
2023-11-06 15:52:32 -1200

    feat update select inactive

--- hero.sql
+++ hero.sql
@@ -153,3 +153,12 @@ VALUES
     --Add column price
     ALTER TABLE public.item
     ADD COLUMN item_price DECIMAL(10, 2);
+
+    -- Update hero 1 to inactive
+UPDATE public.hero
+SET is_active = false
+WHERE hero_id = 1;
+
+-- Delete the item associated with hero 1
+DELETE FROM public.heroitem
+WHERE hero_id = 1;
