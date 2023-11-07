1011b6bd9cada940419a44505922a49d714e3f2e
pjhaymanalo <pjhaymanalo@gmail.com>
2023-11-06 16:01:01 -1200

    feat select heroes archers

--- hero.sql
+++ hero.sql
@@ -168,3 +168,8 @@ SELECT p.player_name, h.hero_name
 FROM public.player p
 INNER JOIN public.hero h ON p.hero_id = h.hero_id
 WHERE h.is_active = true;
+
+--Select hereos archer
+SELECT class_name
+FROM public.class
+WHERE class_name LIKE '%Archers%';
